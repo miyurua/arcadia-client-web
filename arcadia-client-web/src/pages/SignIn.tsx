3c5d288a1f9c3d9267b2d9c8926e3731085556d8
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-05-30 13:57:10 +0530

    add redux toolkit

--- arcadia-client-web/src/pages/SignIn.tsx
+++ arcadia-client-web/src/pages/SignIn.tsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { RootState } from "../redux/store";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state: RootState) => state.user);
 
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -18,7 +26,7 @@ const SignIn = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("api/auth/signin", {
         method: "POST",
         headers: {
@@ -28,15 +36,17 @@ const SignIn = () => {
       });
       const data = await res.json();
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
+      if (error instanceof Error) {
+        dispatch(signInFailure(error.message));
+      } else {
+        dispatch(signInFailure("An unknown error occurred"));
+      }
     }
   };
 
@@ -73,7 +83,7 @@ const SignIn = () => {
           <span className="text-[#91A8ED]">Sign up</span>
         </Link>
       </div>
-      {error && <p className="text-red-500 mt-4">{error}</p>}
+      {error ? <p className="text-red-500 mt-4">{error}</p> : ""}
     </div>
   );
 };
