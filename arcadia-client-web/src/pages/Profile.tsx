550a6a01767c2dbae1849cd8f9a8440ee980d548
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-05-31 19:58:58 +0530

    delete user functionality

--- arcadia-client-web/src/pages/Profile.tsx
+++ arcadia-client-web/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { AiOutlineDelete } from "react-icons/ai";
@@ -10,14 +10,27 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef<HTMLInputElement | null>(null);
-  const { currentUser } = useSelector((state: RootState) => state.user);
+  const { currentUser, loading, error } = useSelector(
+    (state: RootState) => state.user
+  );
   const [file, setFile] = useState(undefined);
   const [uploadPercentage, setUploadPercentage] = useState(0);
   const [uploadError, setUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuceesful, setUpdateSuccessful] = useState(false);
+
+  const dispatch = useDispatch();
   // firebase Storage
 
   useEffect(() => {
@@ -32,8 +45,6 @@ const Profile = () => {
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
 
-    console.log(uploadError);
-
     uploadTask.on(
       "state_changed",
       (snapshot) => {
@@ -42,7 +53,9 @@ const Profile = () => {
         setUploadPercentage(Math.round(progress));
       },
       (error) => {
-        setUploadError(true);
+        if (error instanceof Error) {
+          setUploadError(true);
+        }
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
@@ -52,10 +65,66 @@ const Profile = () => {
     );
   };
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccessful(true);
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(updateUserFailure(error.message));
+      } else {
+        dispatch(
+          updateUserFailure("An unknown error occurred during user update")
+        );
+      }
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      if (error instanceof Error) {
+        dispatch(deleteUserFailure(error.message));
+      } else {
+        dispatch(
+          deleteUserFailure("An unknown error occurred during account deletion")
+        );
+      }
+    }
+  };
+
   return (
     <div className="font-spacemono p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setFile(e.target.files[0])}
           type="file"
@@ -71,7 +140,7 @@ const Profile = () => {
         />
         <p className="mx-auto text-sm">
           {uploadError ? (
-            <span className="text-red-700">
+            <span className="text-red-500">
               Error Image upload (image must be less than 2 mb)
             </span>
           ) : uploadPercentage > 0 && uploadPercentage < 100 ? (
@@ -81,34 +150,49 @@ const Profile = () => {
           ) : (
             " "
           )}
+          {error ? <span className="text-red-500">{error}</span> : ""}
+          {updateSuceesful ? (
+            <span className="text-[#21A193]">Profile Updated Successfully</span>
+          ) : (
+            ""
+          )}
         </p>
         <input
           type="text"
+          defaultValue={currentUser.username}
           placeholder="Username"
           className="border p-3 rounded-md"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
+          defaultValue={currentUser.email}
           placeholder="Email"
           className="border p-3 rounded-md"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           className="border p-3 rounded-md"
           id="password"
+          onChange={handleChange}
         />
         <button
+          disabled={loading}
           className="border rounded-md px-7 py-4 hover:drop-shadow-xl	 hover:bg-[#21A193] hover:text-white hover:font-semibold disabled:bg-red-300
         hover:shadow-[5px_5px_0px_0px_rgba(0,0,0)]"
         >
-          Update
+          {loading ? "Loading..." : "Update"}
         </button>
       </form>
       <div className="flex justify-between mt-6">
-        <span className="cursor-pointer text-[#EA4335] flex items-center gap-1">
+        <span
+          onClick={handleDeleteAccount}
+          className="cursor-pointer text-[#EA4335] flex items-center gap-1"
+        >
           Delete account <AiOutlineDelete />
         </span>
         <span className="cursor-pointer text-[#EA4335] flex items-center gap-1">
