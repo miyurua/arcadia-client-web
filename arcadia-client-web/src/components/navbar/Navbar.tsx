2ad54c6df7c92009d682cbd2bf6045ff615cf0b2
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-06-05 00:06:05 +0530

    update search header

--- arcadia-client-web/src/components/navbar/Navbar.tsx
+++ arcadia-client-web/src/components/navbar/Navbar.tsx
@@ -1,10 +1,30 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { RootState } from "../../redux/store";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
+  const [serachTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", serachTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlSearchTerm = urlParams.get("searchTerm");
+
+    if (urlSearchTerm) {
+      setSearchTerm(urlSearchTerm);
+    }
+  }, [location.search]);
 
   return (
     <header className="bg-[#F9F5F2] border-b">
@@ -14,13 +34,22 @@ const Navbar = () => {
             Arcadia
           </span>
         </Link>
-        <form className="bg-white p-3 rounded-md flex flex-row items-center font-inconsolata">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white p-3 rounded-md flex flex-row items-center font-inconsolata"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={serachTerm}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setSearchTerm(e.target.value)
+            }
           />
-          <FaSearch className="text-[#3B0A45]" />
+          <button>
+            <FaSearch className="text-[#3B0A45]" />
+          </button>
         </form>
         <ul className="flex flex-row gap-4 font-spacemono font-semibold items-center">
           <Link to="/">
