255493c32522d24f3348e6ac083667e911ad1f93
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-06-08 13:20:45 +0530

    start of the home page

--- arcadia-client-web/src/pages/Home.tsx
+++ arcadia-client-web/src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="font-spacemono p-3 max-w-lg mx-auto">
+      <div>All the games in the world!</div>
+    </main>
+  );
 };
 
 export default Home;
